f863b76c74900bbcb94c7060f94b397f967596c7
Erick <erick.kuwahara79@gmail.com>
2023-04-20 16:45:52 -0300

    fix: header

--- src/components/CartButton.tsx
+++ src/components/CartButton.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function CartButton() {
   return (
-    <button className="relative flex items-center justify-center w-12 h-12 rounded-md bg-gray-700">
+    <button className="relative flex items-center justify-center w-12 h-12 rounded-md bg-gray-700 ml-auto">
       <Image src="/bag.svg" alt="bag" width={24} height={24} />
     </button>
   );
--- src/patterns/Header.tsx
+++ src/patterns/Header.tsx
@@ -4,7 +4,7 @@ import CartButton from "@/components/CartButton";
 
 export default function Header() {
   return (
-    <header className="container flex justify-between py-10">
+    <header className="container flex justify-center py-10">
       <a href="/">
         <Image src="./logo.svg" alt="logo" width={130} height={52} priority />
       </a>
