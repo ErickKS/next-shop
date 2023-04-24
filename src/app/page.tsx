7f1c4d7d100377a3f64e3cda5ef73dc313c8830e
Erick <erick.kuwahara79@gmail.com>
2023-04-24 13:58:32 -0300

    feat: product slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,28 @@
+"use client";
+
 import Product from "@/components/Product";
 import Header from "@/patterns/Header";
 
+import useEmblaCarousel from "embla-carousel-react";
+
 export default function Home() {
+  const [emblaRef] = useEmblaCarousel({
+    align: "start",
+    skipSnaps: false,
+    dragFree: true
+  });
+
   return (
     <>
       <Header />
       <main className="container flex items-center justify-between">
-        <Product />
+        <div className="embla" ref={emblaRef}>
+          <div className="embla__container flex gap-12 mx-auto">
+            <Product />
+            <Product />
+            <Product />
+          </div>
+        </div>
       </main>
     </>
   );
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Product() {
   return (
-    <a
+    <Link
       href="/"
-      className="group relative flex items-center justify-center w-[696px] h-[656px] bg-gradient rounded-lg overflow-hidden"
+      className="embla__slide group relative flex items-center justify-center w-[696px] h-[656px] bg-gradient rounded-lg overflow-hidden"
     >
       <Image
         src="/images/img-2.png"
@@ -12,12 +13,12 @@ export default function Product() {
         width={520}
         height={480}
       />
-      <div className="absolute bottom-1 flex justify-between w-[688px] p-5 rounded-lg bg-gray-800 translate-y-16 opacity-0 transition group-hover:translate-y-0 group-hover:opacity-100">
+      <footer className="absolute bottom-1 flex justify-between w-[688px] p-5 rounded-lg bg-gray-800 translate-y-16 opacity-0 transition group-hover:translate-y-0 group-hover:opacity-100">
         <div>
           <h2>Camiseta Maratona Explorer 2.0</h2>
           <span className="text-lg font-bold">R$ 74,90</span>
         </div>
-      </div>
-    </a>
+      </footer>
+    </Link>
   );
 }
